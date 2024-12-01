26c4284755532796dddf6f4fa1fcc9bdf4103520
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2024-12-01 22:16:38 +0100

    some draft solutions for day1 of advent of code 2023

---
+++ day1/day1.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const fileName = 'day1_data.txt';
+
+const data = fs.readFileSync(process.cwd() + '/' + fileName).toString();
+
+const rows = data.split('\n').filter(item => item != '');
+const lefts = [];
+const rights = [];
+
+rows.forEach(r => {
+	const locations = r.split(' ').filter(item => item != '');
+	lefts.push(+locations[0]);
+	rights.push(+locations[1]);
+}); 
+
+lefts.sort();
+rights.sort();
+
+const distances = rows.map((_, index) => {
+	return Math.abs(lefts[index] - rights[index]);
+}); 
+
+const totalDistance = distances.reduce((prev, curr) => prev + curr, 0);
+
+console.log(totalDistance);
