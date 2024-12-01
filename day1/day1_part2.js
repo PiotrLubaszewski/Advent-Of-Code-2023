6af97fe040a3564d4a01a80bf6e436193de061d6
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2024-12-01 22:47:49 +0100

    day 1 two stars

---
+++ day1/day1_part2.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const fileName = 'day1_data.txt';
+
+const data = fs.readFileSync(process.cwd() + '/' + fileName).toString();
+
+const rows = data.split('\n').filter(item => item != '');
+const lefts = [];
+const rights = [];
+
+rows.forEach(r => {
+	const locations = r.split(' ').filter(item => item != '');
+	lefts.push(+locations[0]);
+	rights.push(+locations[1]);
+}); 
+
+const similarity_scores = lefts.map((l, index) => {
+	let score = 0;
+	
+	rights.forEach(r => {
+		if(l === r) { score++ }; 
+	})
+
+	return l * score;
+}) 
+
+const similarity_score = similarity_scores.reduce((prev, curr) => prev + curr, 0);
+
+console.log(similarity_score);
+
+
