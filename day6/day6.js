f7fdcbfaa643d01b3355825a1fb0bcfb44cb668b
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2024-12-08 17:29:49 +0100

    day6 pt2

--- day6/day6.js
+++ day6/day6.js
@@ -14,20 +14,22 @@ const prepareData = (data) => {
     for(let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i].length; j++) {
             const charAtPosition = data[i][j];
+            const x = j + 1;
+            const y = i + 1;
             if(charAtPosition === '#') {
-                barriers.push({x: i, y: j})
+                barriers.push({x, y})
             };
             if(charAtPosition === '^') {
-                cursor = {x: i, y: j, direction: "UP"}
+                cursor = {x, y, direction: "UP"}
             }
             if(charAtPosition === '>') {
-                cursor = {x: i, y: j, direction: "RIGHT"}
+                cursor = {x, y, direction: "RIGHT"}
             }
             if(charAtPosition === 'v') {
-                cursor = {x: i, y: j, direction: "DOWN"}
+                cursor = {x, y, direction: "DOWN"}
             }
             if(charAtPosition === '<') {
-                cursor = {x: i, y: j, direction: "LEFT"}
+                cursor = {x, y, direction: "LEFT"}
             }
         }
     }
@@ -35,28 +37,57 @@ const prepareData = (data) => {
     return { barriers, cursor };
 }
 
+const getVerticalBarriers = (barriers, xPosition) => {
+    return barriers.filter(b => {
+            return b.x === xPosition;
+        })
+}
+
+const getHorizontalBarriers = (yPosition) => {
+     return barriers.filter(b => {
+            return b.y === yPosition;
+        })
+}
+
 const makeMove = (barriers, cursor) => {
+    let distinctPositionsCount = 0;
     if(cursor.direction === 'UP') {
-        // barriers of x < cursor.x;
+        const barriersOnAxis = getVerticalBarriers(barriers, cursor.x);
+        const closestBarrier = barriersOnAxis.filter(boa => {
+           return boa.y < cursor.y;
+        })[0];
+        distinctPositionsCount += cursor.y - closestBarrier.y;
     }
     if(cursor.direction === 'RIGHT') {
-        // barriers of y > cursor.y;
+        const barriersOnAxis = getHorizontalBarriers(barriers, cursor.y);
+        const closestBarrier = barriersOnAxis.filter(boa => {
+           return boa.x > cursor.x;
+        })[0] 
+        distinctPositionsCount += closestBarrier.x - cursor.x;
     }
     if(cursor.direction === 'DOWN') {
-        // barriers of x > cursor.x;
+        const barriersOnAxis = getVerticalBarriers(barriers, cursor.x);
+        const closestBarrier = barriersOnAxis.filter(boa => {
+            return boa.y > cursor.y;
+        })[0]
+        distinctPositionsCount += closestBarrier.y - cursor.y;
+
     }
     if(cursor.direction === 'LEFT') {
-        // barriers of y < cursor.y;
+        const barriersOnAxis = getHorizontalBarriers(barriers, cursor.y);
+        const closestBarrier = barriersOnAxis.filter(boa => {
+           return boa.x < cursor.x;
+        })[0];
+        distinctPositionsCount += cursor.y - closestBarrier.y; 
     }
-    // set new coordinates for cursor => 
-    // repeat until maze is left
-    // count moves
+    return distinctPositionsCount - 1;
 }
 
 const main = () => {
     const data = readData('day6.txt');
     const { barriers, cursor } = prepareData(data);
-    console.log(barriers, cursor);
+    const distance = makeMove(barriers, cursor);
+    console.log(distance);
 }
 
 main();
