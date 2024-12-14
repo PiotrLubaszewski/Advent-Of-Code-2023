5e5edc3c2d069c71b72be8a119ddce46eb6c4f07
Piotr Lubaszewski <piotrlubaszewski@gmail.com>
2024-12-14 23:12:56 +0100

    day 13

---
+++ day13/day13.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const readData = (fileName) => {
+  const filePath = path.join(process.cwd(), 'data', fileName);
+  return fs
+    .readFileSync(filePath, 'utf-8')
+    .split('\n\n')
+    .map((m) => m.match(/\d+/g).map(Number));
+};
+
+const tokens = ([aX, aY, bX, bY, pX, pY]) => {
+  const a = (pX * bY - pY * bX) / (aX * bY - aY * bX);
+  const b = (aX * pY - aY * pX) / (aX * bY - aY * bX);
+
+  return Number.isInteger(a) && Number.isInteger(b) ? a * 3 + b : 0;
+};
+
+const main = () => {
+  const data = readData('day13.txt');
+  const result = data.reduce((acc, machine) => {
+    return acc + tokens(machine);
+  }, 0);
+
+  console.log(result);
+};
+
+main();
